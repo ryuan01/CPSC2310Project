ecb1e351aa9d9c59013e2670f6c1ff5ff2799a8d
Arc Ranges <arc.ranges@live.ca>
2016-03-11 12:29:57 -0800

    Added files via upload

---
+++ DIVA/js.js
@@ -0,0 +1,19 @@
+var username;
+var password;
+
+function getUserInfo()  {
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    
+    document.write("Username: " + username + " \nPassword: " + password);
+}
+
+function searchVehicle() {
+    
+    //TODO
+}
+
+function searchReservation() {
+    
+    //TODO
+}
