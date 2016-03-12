cb6223bef6a5e186008e18b87885a1e7092f1456
Arc Ranges <arc.ranges@live.ca>
2016-03-11 17:44:27 -0800

    Added files via upload

--- DIVA/js.js
+++ DIVA/js.js
@@ -1,19 +1,41 @@
 var username;
 var password;
 
-function getUserInfo()  {
-    username = document.getElementById("username").value;
-    password = document.getElementById("password").value;
+// check if either form is filled, or has valid information
+function validateForm() {
     
-    document.write("Username: " + username + " \nPassword: " + password);
+    var uField = document.forms["login_form"]["username"].value;
+    var pField = document.forms["login_form"]["password"].value;
+    
+    if ((uField == null || pField == null) || (uField == "" || pField == "")) {
+        return false;
+    }
+    return true;
+    
+}
+// get user login credentials
+function getUserInfo() {
+    
+    if (validateForm()) {
+        
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
+    
+        document.getElementById("greetings").innerHTML = "\nUsername: " + username + " \nPassword: " + password;
+    }
+    else {
+        
+        document.getElementById("validator").innerHTML = "Invalid username or password.";
+    }
 }
 
-function searchVehicle() {
-    
-    //TODO
-}
-
-function searchReservation() {
-    
-    //TODO
-}
+// search database for vehicle
+//function searchVehicle() {
+//    
+//    //TODO
+//}
+//
+//function searchReservation() {
+//    
+//    //TODO
+//}
